dc0abf1fa9fed4766b56184a809956c364f7b91a
Nikita Sokolov UA <nsokolov0309@gmail.com>
2022-08-24 17:54:12 +0300

    make second gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const boxGallery = document.querySelector('.gallery')
+const createGalleryItems = galleryItems.map(({original, description, preview}) => `
+    <div class="gallery__item">        
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" 
+            alt="${description}" />
+        </a>
+    </div>`
+).join('')
+
+
+boxGallery.insertAdjacentHTML('afterbegin', createGalleryItems)
+
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionType: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
+
